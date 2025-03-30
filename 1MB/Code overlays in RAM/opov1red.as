8b6104b94ad71b066962c323e91fd917f57c0696
ladislau szilagyi <87603175+Laci1953@users.noreply.github.com>
2025-03-30 07:45:38 +0300

    Delete 1MB directory

--- 1MB/Code overlays in RAM/opov1red.as
+++
@@ -1,9 +0,0 @@
-psect text
-
-global _O_optimise
-;global _O_sub_404d
-;global _O_loadFunction
-
-jp  _O_optimise
-jp 0; _O_sub_404d
-jp 0; _O_loadFunction
--- 1MB/overlays.h
+++
@@ -1,8 +0,0 @@
-void InitOverlays(void);
-short CallOverlay0(void* Routine, char CalledRomBank);
-short CallOverlay1(void* Routine, short Par1, char CalledRomBank);
-short CallOverlay2(void* Routine, short Par1, short Par2, char CalledRomBank);
-void ReturnOverlay0(short ret_val);
-void ReturnOverlay0_(short ret_val);
-void ReturnOverlay1(short ret_val);
-void ReturnOverlay2(short ret_val);
